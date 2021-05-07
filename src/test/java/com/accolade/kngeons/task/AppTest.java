d323009e581938e8f48bf0691770651f1f10d4ff
Jakub Zeman <jakub.zeman@accolade.com>
2021-05-07 17:33:06 +0200

    Added more UNIT tests

--- src/test/java/com/accolade/kngeons/task/AppTest.java
+++ src/test/java/com/accolade/kngeons/task/AppTest.java
@@ -1,13 +1,18 @@
 package com.accolade.kngeons.task;
 
-import com.accolade.kngeons.task.App;
-
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class AppTest {
     @Test
@@ -30,5 +35,33 @@ public class AppTest {
         assertEquals(1, App.solution(Arrays.asList(
                 -1, -3
         )).intValue());
+    }
+
+    private List<Integer> getArrayFromTxtFile(final String fn) throws FileNotFoundException {
+        final List<Integer> A = new ArrayList<>();
+        Path resourceDirectory = Paths.get("src","test","resources", fn);
+        File file = new File(resourceDirectory.toFile().getAbsolutePath());
+        Scanner myReader = new Scanner(file);
+        while (myReader.hasNextLine()) {
+            String line = myReader.nextLine();
+            String inArray[] = line.split(",");
+            for (String i : inArray) {
+                A.add(Integer.parseInt(i.strip()));
+            }
+        }
+        myReader.close();
+        return A;
+    }
+
+    @Test
+    public void testBigNumbers() throws FileNotFoundException {
+        final List<Integer> A = getArrayFromTxtFile("list.txt");
+        assertEquals(50997, App.solution(A).intValue());
+    }
+
+    @Test
+    public void testBigArray() throws FileNotFoundException {
+        final List<Integer> A = getArrayFromTxtFile("list_from_1.txt");
+        assertEquals(100000, App.solution(A).intValue());
     }
 }
